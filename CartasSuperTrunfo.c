e8eae4fdc0440b151b90f16b94f60bfce5459a81
anacristinateodorof-ux <anacristina.teodorof@gmail.com>
2025-11-11 18:45:48 +0000

    aprendendo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -26,19 +26,19 @@ int main() {
     printf("PREENCHA AS INFORMAÇÕES DAS SUAS CARTAS \n");
     printf("-----CARTA 1----- \n" );
     printf("Estado: ");
-    scanf(" %[^\n]" , estado1);
+    scanf(" %[^\n]", estado1);
     printf("Código da carta:");
-    scanf("%s" , codigo1);
+    scanf("%s", codigo1);
     printf("Cidade: ");
-    scanf(" %[^\n]" , cidade1);
+    scanf(" %[^\n]", cidade1);
     printf("População:");
-    scanf("%d" , &populacao1);
+    scanf("%d", &populacao1);
     printf("Aréa (km²):");
-    scanf("%.2f" , &area1);
+    scanf("%f", &area1);
     printf("PIB: ");
-    scanf("%f" , &pib1);
+    scanf("%f", &pib1);
     printf("Pontos Turisticos:");
-    scanf("%d" , &pontos1);
+    scanf("%d", &pontos1);
   
 
 
